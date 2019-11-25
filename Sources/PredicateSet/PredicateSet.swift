a46b68e5eda67dc4334ff498a78413ee08f3532c
Abizer Nasir <abizern@abizern.org>
2019-11-25 15:05:15 +0000

    First attempt

--- Sources/PredicateSet/PredicateSet.swift
+++ Sources/PredicateSet/PredicateSet.swift
@@ -1,3 +1,55 @@
-struct PredicateSet {
-    var text = "Hello, World!"
+public typealias PredicateSet<Element> = (Element) -> Bool
+
+public func empty<Element>() -> PredicateSet<Element> {
+    { _ in return false}
 }
+
+public func universe<Element>() -> PredicateSet<Element> {
+    { _ in return true }
+}
+
+public func pure<Element: Equatable>(_ e: Element) -> PredicateSet<Element> {
+    { x in x == e}
+}
+
+public func elementSet<Element: Equatable>(_ elements: Element...) -> PredicateSet<Element> {
+    { x in elements.contains(x)}
+}
+
+public func elementSet<Element: Equatable>(_ elements: [Element]) -> PredicateSet<Element> {
+    { x in elements.contains(x)}
+}
+
+public func complement<Element>(_ set: @escaping PredicateSet<Element>) -> PredicateSet<Element> {
+    { x in !set(x) }
+}
+
+public func union<Element>(_ lhs: @escaping PredicateSet<Element>, _ rhs: @escaping PredicateSet<Element>) -> PredicateSet<Element> {
+    { x in lhs(x) || rhs(x) }
+}
+
+public func intersection<Element>(_ lhs: @escaping PredicateSet<Element>, _ rhs: @escaping PredicateSet<Element>) -> PredicateSet<Element> {
+    { x in lhs(x) && rhs(x) }
+}
+
+public func difference<Element>(_ lhs: @escaping PredicateSet<Element>, _ rhs: @escaping PredicateSet<Element>) -> PredicateSet<Element> {
+    { x in lhs(x) && complement(rhs)(x) }
+}
+
+public func symmetricDifference<Element>(_ lhs: @escaping PredicateSet<Element>, _ rhs: @escaping PredicateSet<Element>) -> PredicateSet<Element> {
+    { x in union(difference(lhs, rhs), difference(rhs, lhs))(x)}
+}
+
+public func cartesianProduct<T, V>(_ lhs: @escaping PredicateSet<T>, _ rhs: @escaping PredicateSet<V>) -> PredicateSet<(T, V)> {
+    { (arg) -> Bool in
+        let (x, y) = arg
+        return lhs(x) && rhs(y)
+    }
+}
+
+public func contramap<T, V>(_ set: @escaping PredicateSet<T>, _ transform: @escaping (V) -> T) -> PredicateSet<V> {
+    { x in
+        return set(transform(x))
+    }
+}
+
--- Tests/PredicateSetTests/PredicateSetTests.swift
+++ Tests/PredicateSetTests/PredicateSetTests.swift
@@ -1,15 +1,127 @@
 import XCTest
-@testable import PredicateSet
+import PredicateSet
+
+enum Item: Equatable {
+    case a, b, c, d, e, f
+}
 
 final class PredicateSetTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PredicateSet().text, "Hello, World!")
+    func testEmpty() {
+        XCTAssertFalse(empty()(Item.a))
+        XCTAssertFalse(empty()(Item.b))
+        XCTAssertFalse(empty()(Item.c))
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testUniverse() {
+        XCTAssertTrue(universe()(Item.a))
+        XCTAssertTrue(universe()(Item.b))
+        XCTAssertTrue(universe()(Item.c))
+    }
+
+    func testPredicate() {
+        let set: PredicateSet<Int> = { $0.isMultiple(of: 2)}
+        XCTAssertTrue(set(0))
+        XCTAssertFalse(set(1))
+        XCTAssertTrue(set(2))
+        XCTAssertFalse(set(3))
+        XCTAssertTrue(set(4))
+        XCTAssertFalse(set(5))
+    }
+
+    func testPure() {
+        let set = pure(Item.a)
+        XCTAssertTrue(set(.a))
+        XCTAssertFalse(set(.b))
+        XCTAssertFalse(set(.c))
+    }
+
+    func testElements() {
+        let set = elementSet(Item.a, .c)
+        XCTAssertTrue(set(Item.a))
+        XCTAssertFalse(set(.b))
+        XCTAssertTrue(set(.c))
+    }
+
+    func testElementArray() {
+        let set = elementSet([Item.a, .c])
+        XCTAssertTrue(set(Item.a))
+        XCTAssertFalse(set(.b))
+        XCTAssertTrue(set(.c))
+    }
+
+    func testComplement() {
+        let set = pure(Item.b)
+        let complementSet = complement(set)
+        XCTAssertTrue(complementSet(.a))
+        XCTAssertFalse(complementSet(.b))
+        XCTAssertTrue(complementSet(.c))
+    }
+
+    func testUnion() {
+        let lhs = pure(Item.b)
+        let rhs = elementSet(Item.e, .f)
+        let set = union(lhs, rhs)
+        XCTAssertFalse(set(.a))
+        XCTAssertTrue(set(.b))
+        XCTAssertFalse(set(.c))
+        XCTAssertFalse(set(.d))
+        XCTAssertTrue(set(.e))
+        XCTAssertTrue(set(.f))
+    }
+
+    func testIntersection() {
+        let lhs = elementSet(Item.b, .c, .d)
+        let rhs = elementSet(Item.c, .d, .e)
+        let set = intersection(lhs, rhs)
+        XCTAssertFalse(set(.b))
+        XCTAssertTrue(set(.c))
+        XCTAssertTrue(set(.d))
+        XCTAssertFalse(set(.e))
+    }
+
+    func testDifference() {
+        let lhs = elementSet(Item.b, .c, .d)
+        let rhs = elementSet(Item.c, .d, .e)
+        let set = difference(lhs, rhs)
+        XCTAssertFalse(set(.a))
+        XCTAssertTrue(set(.b))
+        XCTAssertFalse(set(.c))
+        XCTAssertFalse(set(.d))
+        XCTAssertFalse(set(.e))
+        XCTAssertFalse(set(.f))
+    }
+
+    func testSymmetricDifference() {
+        let lhs = elementSet(Item.b, .c, .d)
+        let rhs = elementSet(Item.c, .d, .e)
+        let set = symmetricDifference(lhs, rhs)
+        XCTAssertFalse(set(.a))
+        XCTAssertTrue(set(.b))
+        XCTAssertFalse(set(.c))
+        XCTAssertFalse(set(.d))
+        XCTAssertTrue(set(.e))
+        XCTAssertFalse(set(.f))
+    }
+
+    func testCartesianProduct() {
+        let lhs = elementSet(Item.b, .c)
+        let rhs = elementSet(1, 2)
+        let set = cartesianProduct(lhs, rhs)
+        XCTAssertFalse(set((.a, 1)))
+        XCTAssertTrue(set((.b, 1)))
+        XCTAssertTrue(set((.b, 2)))
+        XCTAssertTrue(set((.c, 1)))
+        XCTAssertTrue(set((.c, 2)))
+        XCTAssertFalse(set((.c, 3)))
+    }
+
+    func testContramap() {
+        let original = elementSet("2", "3")
+        let set = contramap(original) { (n: Int) in String(describing: n) }
+        XCTAssertFalse(set(1))
+        XCTAssertTrue(set(2))
+        XCTAssertTrue(set(3))
+        XCTAssertFalse(set(4))
+    }
 }
+
